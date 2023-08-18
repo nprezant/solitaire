4c668c3b123dc87c4c20ed47f715bb182e82ce8d
Noah Prezant <noah.prezant@protonmail.ch>
2023-08-17 22:12:29 -0600

    Update eslint style and add many JSDoc comments

--- app.js
+++ app.js
@@ -1,65 +1,94 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
 // const STORAGE_KEY = 'solitaire'
 
 // const DECK_SIZE = 52
-const SUIT_SIZE = 13
-
-// const cardImg = new Image()
-// cardImg.src = 'img/cards/simple/hearts-4.svg'
-
-// const backImage = new Image()
-// backImage.src = 'img/cards/simple/back.svg'
+const SUIT_SIZE = 13;
 
 // ---------------------
 // Images
 // ---------------------
 
+/**
+ * Manages card images (fronts and backs)
+ */
 class CardImageCache {
-  images = {}
+  images = {};
 
-  constructor (theme) {
-    this.theme = theme
-    this.load()
+  /**
+   * Creates a card image cache for a theme.
+   * @param {string} theme
+   */
+  constructor(theme) {
+    this.theme = theme;
+    this.load();
   }
 
-  getImage (suit, number) {
-    const imageName = this._generateImageName(suit, number)
-    return this.images[imageName]
+  /**
+   * Gets the image for a particular suite and number.
+   * @param {string} suit
+   * @param {integer} number
+   * @return {Image}
+   */
+  getImage(suit, number) {
+    const imageName = this._generateImageName(suit, number);
+    return this.images[imageName];
   }
 
-  getBack () {
-    return this.images.back
+  /**
+   * Gets the image for the back of a card.
+   * @return {Image}
+   */
+  getBack() {
+    return this.images.back;
   }
 
-  _generateImageName (suit, number) {
-    return 'hearts-4'
+  /**
+   * Generates the image name as stored on disk.
+   * @param {string} _suit
+   * @param {string} _number
+   * @return {string}
+   */
+  _generateImageName(_suit, _number) {
+    return 'hearts-4';
     // return suit + '-' + number // TODO uncomment when we have all the images
   }
 
-  _generateImagePath (name) {
-    return 'img/cards/' + this.theme + '/' + name + '.svg'
+  /**
+   * Generates the path of an image in the current theme.
+   * @param {string} name no extension
+   * @return {string}
+   */
+  _generateImagePath(name) {
+    return 'img/cards/' + this.theme + '/' + name + '.svg';
   }
 
-  _setImage (name) {
+  /**
+   * Sets the image with a particular name to the current theme.
+   * @param {string} name no extension
+   */
+  _setImage(name) {
     if (isUndefined(this.images[name])) {
-      this.images[name] = new Image()
+      this.images[name] = new Image();
     }
 
-    const imagePath = this._generateImagePath(name)
-    this.images[name].src = imagePath
+    const imagePath = this._generateImagePath(name);
+    this.images[name].src = imagePath;
   }
 
-  load () {
+  /**
+   * Loads all images for the theme.
+   */
+  load() {
     // Card back image
-    this._setImage('back')
+    this._setImage('back');
 
     // Number value images
     for (const [suit] of Object.entries(Suits)) {
       for (let number = 0; number < SUIT_SIZE; ++number) {
-        const imageName = this._generateImageName(suit, number)
-        this._setImage(imageName)
+        const imageName = this._generateImageName(suit, number);
+        this._setImage(imageName);
       }
     }
   }
@@ -69,200 +98,330 @@ class CardImageCache {
 // Game logic
 // ---------------------
 
-function makeEnum (arr) {
-  const obj = Object.create(null)
+/**
+ * Generates a type that acts a bit like an enum.
+ * @param {string[]} arr enum names
+ * @return {enum}
+ */
+function makeEnum(arr) {
+  const obj = Object.create(null);
   for (const val of arr) {
-    obj[val] = Symbol(val)
+    obj[val] = Symbol(val);
   }
-  return Object.freeze(obj)
+  return Object.freeze(obj);
 }
 
-const Suits = makeEnum(['spades', 'hearts', 'clubs', 'diamonds'])
-const Colors = makeEnum(['red', 'black'])
+const Suits = makeEnum(['spades', 'hearts', 'clubs', 'diamonds']);
+const Colors = makeEnum(['red', 'black']);
 
-function suitColor (suit) {
+/**
+ * Gets the color of a suite.
+ * @param {symbol} suit from the suits enum
+ * @return {symbol} red or black enum color
+ */
+function suitColor(suit) {
   if (suit === Suits.spades || suit === Suits.clubs) {
-    return Colors.black
+    return Colors.black;
   }
-  return Colors.red
+  return Colors.red;
 }
 
+/**
+ * A playing card.
+ */
 class Card {
-  constructor (number, suit) {
-    this.number = number
-    this.suit = suit
-    this.color = suitColor(suit)
+  /**
+   *
+   * @param {number} number card number, 1-13
+   * @param {symbol} suit
+   */
+  constructor(number, suit) {
+    this.number = number;
+    this.suit = suit;
+    this.color = suitColor(suit);
   }
 }
 
-function shuffleArray (array) {
+/**
+ * Algorithm to shuffle an arbitrary array.
+ * @param {array} array any array
+ */
+function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
-function isUndefined (x) {
-  return typeof x === 'undefined'
+/**
+ * Conveneince for checking if a value is undefined.
+ * @param {any} x any value
+ * @return {boolean}
+ */
+function isUndefined(x) {
+  return typeof x === 'undefined';
 }
 
+/**
+ * A deck of cards.
+ * Or a stack of cards.
+ * Supports adding, removing, peeking, shuffling.
+ */
 class Deck {
-  #cards = []
+  #cards = [];
 
-  static full () {
-    const deck = new Deck()
+  /**
+   * Static constructor for a full deck of 52 cards.
+   * Unshuffled.
+   * @return {Deck}
+   */
+  static full() {
+    const deck = new Deck();
     for (const [suit] of Object.entries(Suits)) {
       for (let number = 0; number < SUIT_SIZE; number++) {
-        deck.addToBottom(new Card(number, suit))
+        deck.addToBottom(new Card(number, suit));
       }
     }
+    return deck;
   }
 
-  shuffle () {
-    shuffleArray(this.#cards)
+  /**
+   * Shuffles the deck.
+   */
+  shuffle() {
+    shuffleArray(this.#cards);
   }
 
-  // Returns the first element in the array.
-  // Does not modify the deck.
-  // Undefined if the array is empty.
-  peek () {
-    return this.#cards[0]
+  /**
+   * Returns the first element in the array.
+   * Does not modify the deck.
+   * Undefined if the array is empty.
+   * @return {Card}
+   */
+  peek() {
+    return this.#cards[0];
   }
 
-  take (nCards) {
-    const taken = []
+  /**
+   * Takes cards from the top of the deck.
+   * @param {number} nCards
+   * @return {Card[]} cards taken out of this deck
+   */
+  take(nCards) {
+    const taken = [];
     for (let i = 0; i < nCards; ++i) {
-      taken.push(this.#cards.shift())
+      taken.push(this.#cards.shift());
     }
     // When we run out of cards we will get undefined values.
-    taken.filter((x) => isUndefined(x))
-    return taken
+    taken.filter((x) => isUndefined(x));
+    return taken;
   }
 
-  addToBottom (cards) {
-    this.#cards.push(cards)
+  /**
+   * Add cards to the bottom of the deck.
+   * @param {Card[]} cards
+   */
+  addToBottom(cards) {
+    this.#cards.push(cards);
   }
 
-  addToTop (cards) {
-    this.#cards.unshift(cards)
+  /**
+   * Add cards to the top of the deck.
+   * @param {Card[]} cards
+   */
+  addToTop(cards) {
+    this.#cards.unshift(cards);
   }
 
-  empty () {
-    return this.#cards.length === 0
+  /**
+   * True if there are no cards in the deck.
+   * @return {boolean}
+   */
+  empty() {
+    return this.#cards.length === 0;
   }
 }
 
+/**
+ * A single foundation.
+ * In solitaire, a foundation is one of the piles at the top
+ * that starts with an Ace and builds upwards.
+ */
 class Foundation {
-  #cards = new Deck()
+  #cards = new Deck();
 
-  isPlayable (card) {
-    const topCard = this.#cards.peek()
+  /**
+   * Whether or not a card is playable on this foundation.
+   * @param {Card} card card to test
+   * @return {boolean}
+   */
+  isPlayable(card) {
+    const topCard = this.#cards.peek();
 
     if (isUndefined(topCard) && card.number === 1) {
       // Ace
-      return true
+      return true;
     }
 
     if (topCard.suit === card.suit && topCard.number === card.number - 1) {
-      return true
+      return true;
     }
 
-    return false
+    return false;
   }
 
-  ncards () {
-    return this.#cards.length
+  /**
+   * Number of cards in the stack.
+   * @return {number}
+   */
+  ncards() {
+    return this.#cards.length;
   }
 }
 
+/**
+ * The set of foundations (one for each suit)
+ * At the top of the game board.
+ */
 class Foundations {
-  #foundations = [] // Decks
+  #foundations = [];
 
-  constructor () {
+  /**
+   * Initializer.
+   */
+  constructor() {
     for (const [,] of Object.entries(Suits)) {
-      this.#foundations.push(new Foundation())
+      this.#foundations.push(new Foundation());
     }
   }
 
-  isPlayable (card) {
-    // Can this card be played on a foundation?
-    // Returns indexes of foundations that it is playable on.
-    const playable = Array(this.#foundations.length).fill(false)
+  /**
+   * Checks which if any foundations this card can be played on.
+   * @param {Card} card card to test
+   * @return {boolean[]} true/false mask of playable foundations.
+   */
+  isPlayable(card) {
+    const playable = Array(this.#foundations.length).fill(false);
 
     for (const [i, foundation] of Object.entries(this.#foundations)) {
-      playable[i] = foundation.isPlayable(card)
+      playable[i] = foundation.isPlayable(card);
     }
 
-    return playable
+    return playable;
   }
 
-  full () {
+  /**
+   * Tests whether the foundations are full.
+   * @return {boolean} true if the foundations are full.
+   */
+  full() {
     for (const [, foundation] of Object.entries(this.#foundations)) {
       if (foundation.ncards() !== SUIT_SIZE) {
-        return false
+        return false;
       }
     }
-    return true
+    return true;
   }
 }
 
+/**
+ * A single column of the tableau.
+ */
 class TableauColumn {
-  #facedown = new Deck()
-  #faceup = new Deck()
+  #facedown = new Deck();
+  #faceup = new Deck();
 
-  isPlayable (card) {
-    const topCard = this.topCard()
+  /**
+   * Whether or not a card can be played on this column
+   * @param {Card} card card to test
+   * @return {boolean}
+   */
+  isPlayable(card) {
+    const topCard = this.topCard();
 
     if (card.color !== topCard.color && card.number === topCard.number - 1) {
-      return true
+      return true;
     }
 
-    return false
+    return false;
   }
 
-  topCard () {
-    return this.#faceup.peek()
+  /**
+   * The topmost card in the column.
+   * This card can either be moved or have the next lower number card
+   * of the opposite color played on it.
+   * @return {Card}
+   */
+  topCard() {
+    return this.#faceup.peek();
   }
 }
 
+/**
+ * The tableau is where the 'solitaire' part of the game happens.
+ * It takes up the majority of the board.
+ * Cards are played downwards in alternating colors.
+ */
 class Tableau {
-  #ncolumns = 5
-  #columns = []
+  #ncolumns = 5;
+  #columns = [];
 
-  constructor () {
+  /**
+   * Initializer
+   */
+  constructor() {
     for (let i = 0; i < this.#ncolumns; ++i) {
-      this.#columns.push(new TableauColumn())
+      this.#columns.push(new TableauColumn());
     }
   }
 
-  isPlayable (card) {
-    const playable = Array(this.#columns.length).fill(false)
+  /**
+   * Checks which if any columns a card is playable on.
+   * @param {Card} card card to test
+   * @return {boolean[]} true/false mask of playable columns.
+   */
+  isPlayable(card) {
+    const playable = Array(this.#columns.length).fill(false);
 
     for (const [i, column] of Object.entries(this.#columns)) {
-      playable[i] = column.isPlayable(card)
+      playable[i] = column.isPlayable(card);
     }
 
-    return playable
+    return playable;
   }
 }
 
+/**
+ * The main solitaire game.
+ */
 class Game {
-  constructor (drawRate) {
-    this.drawRate = drawRate
+  /**
+   * Initializer
+   * @param {number} drawRate how many cards to draw at a time.
+   */
+  constructor(drawRate) {
+    this.drawRate = drawRate;
 
-    this.drawPile = Deck.full() // Cards
-    this.wastePile = new Deck() // Cards
-    this.foundations = new Foundations() // Where the aces stack upwards
-    this.tableau = new Tableau() // Where you play solitaire
+    this.drawPile = Deck.full(); // Cards
+    this.wastePile = new Deck(); // Cards
+    this.foundations = new Foundations(); // Where the aces stack upwards
+    this.tableau = new Tableau(); // Where you play solitaire
   }
 
-  drawStep () {
-    const drawnCards = this.drawPile.take(this.drawRate)
-    this.wastePile.addToTop(drawnCards)
+  /**
+   * Perform the draw step of solitaire.
+   */
+  drawStep() {
+    const drawnCards = this.drawPile.take(this.drawRate);
+    this.wastePile.addToTop(drawnCards);
   }
 
-  won () {
-    return this.foundations.full()
+  /**
+   * Whether the win condition has been met.
+   * @return {boolean}
+   */
+  won() {
+    return this.foundations.full();
   }
 }
 
@@ -270,76 +429,110 @@ class Game {
 // UI
 // ---------------------
 
-const game = new Game()
-const cardImages = new CardImageCache('simple')
+const game = new Game();
+const cardImages = new CardImageCache('simple');
 
-const sampleCard = new Card(4, Suits.hearts)
+const sampleCard = new Card(4, Suits.hearts);
 
-function drawDrawPile () {
-  CardView.draw(300, 50, sampleCard)
+/**
+ * Draws the draw pile on the game board.
+ */
+function drawDrawPile() {
+  CardView.draw(300, 50, sampleCard);
 }
 
-function drawWastePile () {
-  CardView.draw(200, 50, sampleCard)
+/**
+ * Draws the waste pile on the game board.
+ */
+function drawWastePile() {
+  CardView.draw(200, 50, sampleCard);
 }
 
-function drawFoundations () {
-  CardView.draw(50, 50, sampleCard)
+/**
+ * Draws the foundation portion of the game board.
+ */
+function drawFoundations() {
+  CardView.draw(50, 50, sampleCard);
 }
 
-function drawTableau () {
-  CardView.draw(500, 200, sampleCard)
+/**
+ * Draws the tableau portion of the game board.
+ */
+function drawTableau() {
+  CardView.draw(500, 200, sampleCard);
 }
 
-function drawGame () {
+/**
+ * Draws the entire game.
+ */
+function drawGame() {
   if (game.won()) {
-    console.log('we won')
+    console.log('we won');
   }
 
-  drawDrawPile()
-  drawWastePile()
-  drawFoundations()
-  drawTableau()
+  drawDrawPile();
+  drawWastePile();
+  drawFoundations();
+  drawTableau();
 }
 
+/**
+ * A view of a card.
+ */
 class CardView {
-  static width = 50
-  static height = this.width * 89 / 59
-  static cornerRadius = 5
+  static width = 50;
+  static height = this.width * 89 / 59;
+  static cornerRadius = 5;
 
-  static draw (x, y, card) {
-    ctx.beginPath()
-    ctx.roundRect(x, y, this.width, this.height, this.cornerRadius)
-    ctx.strokeStyle = '#ccc'
-    ctx.stroke()
+  /**
+   * Draws a card.
+   * @param {number} x screen coordinate
+   * @param {number} y screen coordinate
+   * @param {Card} card card to draw
+   */
+  static draw(x, y, card) {
+    ctx.beginPath();
+    ctx.roundRect(x, y, this.width, this.height, this.cornerRadius);
+    ctx.strokeStyle = '#ccc';
+    ctx.stroke();
 
-    const img = cardImages.getImage(card.suit, card.number)
-    ctx.drawImage(img, x, y, this.width, this.height)
-    ctx.closePath()
+    const img = cardImages.getImage(card.suit, card.number);
+    ctx.drawImage(img, x, y, this.width, this.height);
+    ctx.closePath();
   }
 }
 
-onresize = (event) => {
-  setCanvasSize()
+onresize = (_event) => {
+  setCanvasSize();
+};
+
+/**
+ * Matches the canvas to the window size.
+ * Useful when the window resizes.
+ */
+function setCanvasSize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
 
-function setCanvasSize () {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
+/**
+ * Startup.
+ */
+function init() {
+  setCanvasSize();
 }
 
-function init () {
-  setCanvasSize()
+/**
+ * Main loop.
+ */
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGame();
+  requestAnimationFrame(draw);
 }
 
-function draw () {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  drawGame()
-  requestAnimationFrame(draw)
-}
-
-init()
-draw()
+init();
+draw();
 
 /*
 // Listen to form submissions.
