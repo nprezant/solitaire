eea5c06f9ff5c07023cf3c24c2d3c5a754e5340e
Noah Prezant <noah.prezant@protonmail.ch>
2023-08-24 23:54:21 -0600

    Rename default ionic app name to solitaire

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'blank-app',
+  appName: 'solitaire',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
