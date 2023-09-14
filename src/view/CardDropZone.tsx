dde515095bc77f8f1ef23797a175d1bf0a0e9c0f
Noah Prezant <noah.prezant@protonmail.ch>
2023-09-13 22:45:40 -0600

    fixup! Refine dragging

--- src/view/CardDropZone.tsx
+++ src/view/CardDropZone.tsx
@@ -2,7 +2,6 @@ import CardLocation from "../model/solitaire/CardLocation";
 
 class CardDropZone extends Phaser.GameObjects.Zone {
 
-  // Location on the board
   public location: CardLocation;
 
   constructor(scene: Phaser.Scene, location: CardLocation, x: number, y: number, width?: number, height?: number) {
@@ -10,17 +9,6 @@ class CardDropZone extends Phaser.GameObjects.Zone {
     scene.add.existing(this);
 
     this.location = location;
-  }
-
-  public bringToTop() {
-    this.scene.children.bringToTop(this);
-  }
-
-  public didDragTo(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-
-    // TODO also update drag dependencies
   }
 }
 
