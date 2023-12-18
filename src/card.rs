7397a759f428de479f41497901c6888d531f2311
Noah Prezant <noah.prezant@protonmail.ch>
2023-12-18 01:15:53 -0500

    Fenagle some svgs

--- src/card.rs
+++ src/card.rs
@@ -3,6 +3,9 @@ use strum::IntoEnumIterator;
 
 use yew::{html, Html}; // 0.17.1
 
+const RAW_SVG_CARD_WIDTH: f32 = 167.5;
+const RAW_SVG_CARD_HEIGHT: f32 = 245.0;
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Card {
     rank: i32,
@@ -24,7 +27,7 @@ impl Card {
     pub fn new_deck() -> Vec<Card> {
         let mut deck = Vec::new();
         for suit in Suit::iter() {
-            for rank in 1..13 {
+            for rank in 1..14 {
                 deck.push(Card::new(rank, suit))
             }
         }
@@ -35,16 +38,40 @@ impl Card {
         let raw_rank = self.rank.to_string();
         let card_rank = match self.rank {
             1..=10 => raw_rank.as_str(),
-            11 => "J",
-            12 => "Q",
-            13 => "K",
+            11 => "jack",
+            12 => "queen",
+            13 => "king",
             _ => "1", // Don't panic
         };
-        let mut card_name = format!("{}_{}", self.suit, card_rank);
-        card_name.make_ascii_lowercase();
-        let classes = format!("card {}", card_name);
+        let mut card_label = format!("{}_{}", card_rank, self.suit);
+        card_label.make_ascii_lowercase();
+        let card_href = format!("img/cards.svg#{}", card_label);
+
+        let row = match self.suit {
+            Suit::Club => 0,
+            Suit::Diamond => 1,
+            Suit::Heart => 2,
+            Suit::Spade => 3,
+        } - 1;
+        let col = self.rank - 1;
+
+        let vb_top = row as f32 * RAW_SVG_CARD_HEIGHT;
+        let vb_left = col as f32 * RAW_SVG_CARD_WIDTH;
+        let vb = format!(
+            "{} {} {} {}",
+            vb_left, vb_top, RAW_SVG_CARD_WIDTH, RAW_SVG_CARD_HEIGHT,
+        );
+        let width = format!("{}", RAW_SVG_CARD_WIDTH);
+        let height = format!("{}", RAW_SVG_CARD_HEIGHT);
+
         let positions = format!("top: {}vw; left: {}vw;", self.top, self.left);
 
-        html! { <div class={classes} style={positions} /> }
+        html! {
+        <div class="card">
+            <svg viewBox={vb} {width} {height} >
+                < use href={card_href} />
+            </svg>
+        </div>
+        }
     }
 }
